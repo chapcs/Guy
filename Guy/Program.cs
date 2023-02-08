1ab02771b7e9557744982fa2ffb23c2fe9b7cdba
cozz-py <bencozzens13@gmail.com>
2023-02-08 08:01:29 -0500

    Called the object's methods

--- Guy/Program.cs
+++ Guy/Program.cs
@@ -12,9 +12,32 @@
             joe.WriteMyInfo();
             bob.WriteMyInfo();
             Console.Write("Enter an amount: ");
-
+            string howMuch = Console.ReadLine();
+            if (howMuch == "") return;
+            if (int.TryParse(howMuch, out int n))
+            {
+                Console.Write("Who should give the cash: ");
+                string whichGuy = Console.ReadLine();
+                if (whichGuy == "Joe")
+                {
+                    int cashGiven = joe.GiveCash(n);
+                    bob.ReceiveCash(cashGiven);
+                }
+                else if (whichGuy == "Bob")
+                {
+                    int cashGiven = bob.GiveCash(n);
+                    joe.ReceiveCash(cashGiven);
+                }
+                else
+                {
+                    Console.WriteLine("Please enter 'Joe' or 'Bob'");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter an amount (or a blank line to exit).");
+            }
         }
-            Console.WriteLine("Hello, World!");
     }
 }
 
